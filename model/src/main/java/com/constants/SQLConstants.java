7747c2b341db78cddae1734bea6b8420c4e2df25
Shreyas <shreyaskunjalchandrahas@gmail.com>
2019-06-30 12:04:17 +0530

    Keywords search.

--- model/src/main/java/com/constants/SQLConstants.java
+++ model/src/main/java/com/constants/SQLConstants.java
@@ -244,8 +244,8 @@ public interface SQLConstants {
     public String DELETE_ARTIST = "DELETE FROM ARTIST_MASTER WHERE ARTIST_ID=? ";
     public String DELETE_ARTIST_MEDIA = "DELETE FROM ARTIST_MEDIA WHERE ARTIST_ID=? ";
 
-    public String SEARCH_PRODUCT_3 = "SELECT * FROM product_master  where concat_ws(' ', `PRODUCT_NAME`, `BRAND_NAME`) REGEXP ? ";
-    public String SEARCH_PRODUCT = "select * from (SELECT * FROM product_master  where MATCH(PRODUCT_NAME) AGAINST(? ) UNION SELECT * FROM product_master  where MATCH(BRAND_NAME) AGAINST(? )UNION SELECT * FROM product_master  where MATCH(DESCRIPTION) AGAINST(? )) as t1  ";
+    public String SEARCH_PRODUCT_3 = "SELECT * FROM product_master  where concat_ws(' ', `PRODUCT_NAME`, `BRAND_NAME`, `keywords`) REGEXP ? ";
+    public String SEARCH_PRODUCT = "select * from (SELECT * FROM product_master  where MATCH(PRODUCT_NAME) AGAINST(? ) UNION SELECT * FROM product_master  where MATCH(BRAND_NAME) AGAINST(? )UNION SELECT * FROM product_master  where MATCH(DESCRIPTION) AGAINST(? ) UNION SELECT * FROM product_master  where MATCH(keywords) AGAINST(? )) as t1  ";
     public String CREATE_PRODUCT = "INSERT INTO product_master( `product_name`, `brand_name`, `website`, `picture`, `shop_link_1`, `shop_link_2`, `shop_link_3`, `shop_link_4`, `shop_link_5`, `description`,general_category,sku,keywords) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)";
     public String UPDATE_PRODUCT = "UPDATE product_master set  `product_name`=?, `brand_name`=?, `website`=?, `picture`=?, `shop_link_1`=?, `shop_link_2`=?, `shop_link_3`=?, `shop_link_4`=?, `shop_link_5`=?, `description`=? , general_category= ?,sku=?,keywords=? where `product_id` = ?";
     public String DELETE_PRODUCT = "DELETE FROM product_master WHERE PRODUCT_ID=? ";
